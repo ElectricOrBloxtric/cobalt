ca700d1ace74a7c0c1eb27bcf02799ab67b058b2
Electric <electricraichugv@gmail.com>
2024-08-07 14:41:22 -0400

    Update rumble.js

--- src/modules/processing/services/rumble.js
+++ src/modules/processing/services/rumble.js
@@ -1,1 +1,19 @@
+export default async function(obj) {
+    try {
+        let response = await fetch(`https://rumble.com/service.php?name=media.details&url=${obj.id}`);
+        let data = await response.json();
 
+        if (data.videos && data.videos.length > 0) {
+            let videoUrl = data.videos[0].url;
+            return {
+                urls: videoUrl.replace("http://", "https://"),
+                filename: `rumble_${obj.id}.mp4`,
+                audioFilename: `rumble_${obj.id}_audio`
+            };
+        } else {
+            return { error: 'ErrorNoVideosFound' };
+        }
+    } catch (error) {
+        return { error: 'ErrorFetchingData' };
+    }
+}
